33a7e73e7bec8a04dc50f86aa9ad432f6d572354
Jordan MacDonald <jordan@wastedintelligence.com>
2015-01-04 23:24:11 -0500

    Initial index population implementation.

--- src/index.rs
+++ src/index.rs
@@ -1,3 +1,7 @@
+use std::io::fs;
+use std::io::fs::PathExtensions;
+use std::str::StrExt;
+
 struct Index {
     path: Path,
     entries: Vec<Path>,
@@ -6,6 +10,45 @@ struct Index {
 impl Index {
     fn new(path: &str) -> Index {
         return Index { path: Path::new(path.to_string()), entries: vec![] }
+    }
+
+    fn populate(&mut self) {
+        let mut directories: fs::Directories;
+        let prefix_length = self.path.as_str().unwrap().len()+1;
+
+        // Start by getting any files in the root path.
+        match fs::readdir(&self.path) {
+            Ok(entries) => {
+                // Put all of the file-based Path entries into the index.
+                for file in entries.iter().filter(|entry| entry.is_file()) {
+                    // Make the file path relative to the index path by stripping it from its string.
+                    let file_path = Path::new(file.as_str().unwrap().slice_from(prefix_length));
+                    self.entries.push(file_path);
+                }
+            },
+            Err(e) => (),
+        }
+
+        // Get an iterator that'll let us walk all of the subdirectories of the index path.
+        match fs::walk_dir(&self.path) {
+            Ok(iterator) => directories = iterator,
+            Err(e) => return,
+        }
+
+        for directory in directories {
+            // Read the subdirectory entries.
+            match fs::readdir(&directory) {
+                Ok(entries) => {
+                    // Put all of the file-based Path entries into the index.
+                    for file in entries.iter().filter(|entry| entry.is_file()) {
+                        // Make the file path relative to the index path by stripping it from its string.
+                        let file_path = Path::new(file.as_str().unwrap().slice_from(prefix_length));
+                        self.entries.push(file_path);
+                    }
+                },
+                Err(e) => continue,
+            }
+        }
     }
 }
 
@@ -29,5 +72,15 @@ fn new_creates_index_with_empty_array() {
     let index = Index::new(path);
     let empty_array: Vec<Path> = vec![];
 
-    assert!(index.entries == empty_array)
+    assert!(index.entries == empty_array);
+}
+
+#[test]
+fn populate_adds_all_files_to_entries() {
+    let path = "tests/sample";
+    let index = &mut Index::new(path);
+    let expected_array = vec![Path::new("root_file"), Path::new("directory/nested_file")];
+    index.populate();
+
+    assert!(index.entries == expected_array);
 }
