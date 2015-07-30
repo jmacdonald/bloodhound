fcf6be3aa38995cf7974c284747fc375f5f95721
Jordan MacDonald <jordan@wastedintelligence.com>
2015-07-30 00:13:36 -0400

    Add a benchmark test for edit_distance function.

---
+++ benches/matching.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+
+extern crate test;
+extern crate bloodhound;
+
+use test::Bencher;
+use std::path::PathBuf;
+use bloodhound::matching::find;
+
+#[bench]
+fn bench_edit_distance(b: &mut Bencher) {
+    let haystack = vec![PathBuf::from("src/hound.rs"),
+        PathBuf::from("lib/hounds.rs"), PathBuf::from("Houndfile")];
+    b.iter(|| find("match", &haystack, 5));
+}
