6ec3e7fb78b42e20dbea33e2eb5259bd355c1c2a
Jordan MacDonald <jordan@wastedintelligence.com>
2017-10-29 22:57:32 -0400

    Add index find method benchmark

---
+++ benches/index.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+
+extern crate test;
+extern crate bloodhound;
+
+use test::Bencher;
+use std::path::PathBuf;
+use bloodhound::Index;
+
+#[bench]
+fn bench_find(b: &mut Bencher) {
+    let mut index = Index::new(PathBuf::from("."));
+    index.populate(None);
+    b.iter(|| index.find("match", 5, true));
+}
